1a659d375ba288506521d56042325bee50fff64b
jonah <ju374@nyu.edu>
2020-06-01 01:45:30 -0500

    token regex

--- tokenizer.h
+++ tokenizer.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <cctype>
+#include <stdexcept>
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <regex>
 #include <string>
 #include <vector>
 
 namespace proj2 {
 
+std::regex const keyword_regex      ("^(struct|int|double|long|char|void)$");
+std::regex const symbol_regex       ("^(\"|'|\\(|\\)|\\{|\\}|;)$"); // " ' ( ) { } ;
+std::regex const identifier_regex   ("^([a-zA-Z_]+\\w*)$");
 
 struct base_token {
     std::string value;
@@ -28,21 +33,36 @@ using token_list = std::vector<base_token>;
 std::unique_ptr<token_list> tokenize(std::ifstream& ifs) {
     std::unique_ptr<token_list> my_tokens;
     char c;
+    char c_str[] = { '\0', '\0' };
     std::string token;
+    token.reserve(30);
     while(!ifs.eof()) {
         ifs.get(c);
+        c_str[0] = c;
         if (std::isspace(c)) {
+            // TODO: clean up this duplicated code
             if (!token.empty()) {
-                std::cout << token << std::endl;
+                if (std::regex_match(token, keyword_regex)) {
+                    std::cout << "keyword " << token << std::endl;
+                } else if (std::regex_match(token, identifier_regex)) {
+                    std::cout << "identifier " << token << std::endl;
+                } else {
+                    throw std::invalid_argument("invalid token"); 
+                }
                 token.clear();
             }
-        } else if (c == ';') {
-            std::cout << token << std::endl;
-            token.clear();
-            // token+=c;
-            // std::cout << token << std::endl;
-            // token.clear();
-            std::cout << c << std::endl;
+        } else if (std::regex_match(c_str, symbol_regex)) {
+            if (!token.empty()) {
+                if (std::regex_match(token, keyword_regex)) {
+                    std::cout << "keyword " << token << std::endl;
+                } else if (std::regex_match(token, identifier_regex)) {
+                    std::cout << "identifier " << token << std::endl;
+                } else {
+                    throw std::invalid_argument("invalid token"); 
+                }
+                token.clear();
+            }
+            std::cout << "symbol " << c << "\n";
         } else {
             token+=c;
         }
