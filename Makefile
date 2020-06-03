81d1fabbbdaccb1a06d8dc7b98b58255fc729a8d
jonah <ju374@nyu.edu>
2020-06-03 01:48:46 -0500

    tokenizer improvements

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 jit:
 	g++ -std=c++17 -Wall -O3 run.cpp -o run.o && ./run.o examples/simple.h
+
+debug:
+	g++ -std=c++17 -Wall -g run.cpp -o run.o
--- tokenizer.h
+++ tokenizer.h
@@ -7,68 +7,76 @@
 #include <memory>
 #include <regex>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace proj2 {
 
-std::regex const keyword_regex      ("^(struct|int|double|long|char|void)$");
-std::regex const symbol_regex       ("^(\"|'|\\(|\\)|\\{|\\}|;)$"); // " ' ( ) { } ;
-std::regex const identifier_regex   ("^([a-zA-Z_]+\\w*)$");
+std::regex const keyword_regex      ("^struct|int|double|long|char|void$");
+std::regex const symbol_regex       ("^\"|'|\\(|\\)|\\{|\\}|;$"); // " ' ( ) { } ;
+std::regex const identifier_regex   ("^[a-zA-Z_]+\\w*$");
 
 struct base_token {
     std::string value;
+    base_token(std::string&& _value) : value(_value) {}
+    base_token(char _value)          : value{_value} {}
 };
 
 struct keyword_token : base_token {
+    keyword_token(std::string&& _value) : base_token(std::move(_value)) {}
 };
 
 struct symbol_token : base_token {
+    symbol_token(std::string&& _value) : base_token(std::move(_value)) {}
+    symbol_token(char _value)          : base_token(_value)            {}
 };
 
 struct identifier_token : base_token {
+    identifier_token(std::string&& _value) : base_token(std::move(_value)) {}
 };
 
-using token_list = std::vector<base_token>;
+using token_list = std::vector<std::unique_ptr<base_token>>;
+
+void fill_token_list(std::unique_ptr<token_list>& my_tokens, std::string& token) {
+    if (!token.empty()) {
+        if (std::regex_match(token, keyword_regex)) {
+            std::cout << "keyword " << token << std::endl;
+            my_tokens->emplace_back(std::make_unique<keyword_token>(std::move(token)));
+        } else if (std::regex_match(token, identifier_regex)) {
+            std::cout << "identifier " << token << std::endl;
+            my_tokens->emplace_back(std::make_unique<identifier_token>(std::move(token)));
+        } else {
+            std::cerr << "invalid token: '" << token << "'\n"; 
+            throw std::invalid_argument("invalid token"); 
+        }
+        token.clear();
+    }
+}
+
+void fill_token_list(std::unique_ptr<token_list>& my_tokens, char token) {
+    std::cout << "symbol " << token << "\n";
+    my_tokens->emplace_back(std::make_unique<symbol_token>(token));
+}
 
 std::unique_ptr<token_list> tokenize(std::ifstream& ifs) {
-    std::unique_ptr<token_list> my_tokens;
-    char c;
-    char c_str[] = { '\0', '\0' };
+    std::unique_ptr<token_list> my_tokens = std::make_unique<token_list>();
+    char next_ch_str[] = { '\0', '\0' };
+    char& next_ch = next_ch_str[0];
     std::string token;
     token.reserve(30);
     while(!ifs.eof()) {
-        ifs.get(c);
-        c_str[0] = c;
-        if (std::isspace(c)) {
-            // TODO: clean up this duplicated code
-            if (!token.empty()) {
-                if (std::regex_match(token, keyword_regex)) {
-                    std::cout << "keyword " << token << std::endl;
-                } else if (std::regex_match(token, identifier_regex)) {
-                    std::cout << "identifier " << token << std::endl;
-                } else {
-                    throw std::invalid_argument("invalid token"); 
-                }
-                token.clear();
-            }
-        } else if (std::regex_match(c_str, symbol_regex)) {
-            if (!token.empty()) {
-                if (std::regex_match(token, keyword_regex)) {
-                    std::cout << "keyword " << token << std::endl;
-                } else if (std::regex_match(token, identifier_regex)) {
-                    std::cout << "identifier " << token << std::endl;
-                } else {
-                    throw std::invalid_argument("invalid token"); 
-                }
-                token.clear();
-            }
-            std::cout << "symbol " << c << "\n";
+        ifs.get(next_ch);
+        if (std::isspace(next_ch)) {
+            fill_token_list(my_tokens, token);
+        } else if (std::regex_match(next_ch_str, symbol_regex)) {
+            fill_token_list(my_tokens, token);
+            fill_token_list(my_tokens, next_ch);
         } else {
-            token+=c;
+            token+=next_ch;
         }
     }
     ifs.close();
-    return nullptr;
+    return my_tokens;
 }
 
 }
